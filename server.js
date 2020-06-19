3fbcd7f7f1b241ad555762e3a53217a1e8b84851
AntonDeXy <podolyananton@gmail.com>
2020-06-19 16:16:26 +0300

    Finished React

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema')
 const app = express()
+const cors = require('cors')
+const path = require('path')
+
+// Allow cross-origin
+
+app.use(cors([
+  {origin: 'http://localhost:3000'}
+]))
 
 app.use(
   '/graphql',
@@ -10,6 +18,12 @@ app.use(
     graphiql: true,
   })
 )
+
+app.use(express.static('public'))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
 
 const PORT = process.env.PORT || 5000
 
